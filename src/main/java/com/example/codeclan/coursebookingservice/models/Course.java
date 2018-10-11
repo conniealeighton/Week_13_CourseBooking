29e45c6d6f7aff27834b63623872ffd0db22a9d9
Connie Leighton <constancealeighton@gmail.com>
2018-10-11 11:22:41 +0100

    created models

---
+++ src/main/java/com/example/codeclan/coursebookingservice/models/Course.java
@@ -0,0 +1,70 @@
+package com.example.codeclan.coursebookingservice.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.awt.print.Book;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "star_rating")
+    private int starRating;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int starRating) {
+        this.name = name;
+        this.town = town;
+        this.starRating = starRating;
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+}
