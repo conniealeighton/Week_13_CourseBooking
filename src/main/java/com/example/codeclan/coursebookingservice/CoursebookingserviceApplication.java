b135d02213221a9a0d7bc53c8818153c7e615b18
Connie Leighton <constancealeighton@gmail.com>
2018-10-11 11:03:17 +0100

    init commit

---
+++ src/main/java/com/example/codeclan/coursebookingservice/CoursebookingserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.codeclan.coursebookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursebookingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursebookingserviceApplication.class, args);
+	}
+}
